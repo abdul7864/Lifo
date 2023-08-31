a89279854fe32f822cb2c90952dfc3efeea06da1
abdul7864 <143673148+abdul7864@users.noreply.github.com>
2023-08-31 05:50:59 -0400

    added index.html

--- script.js
+++ script.js
@@ -1,111 +1,212 @@
-console.log("Welcome to Spotify");
+let now_playing = document.querySelector('.now-playing');
+let track_art = document.querySelector('.track-art');
+let track_name = document.querySelector('.track-name');
+let track_artist = document.querySelector('.track-artist');
 
-// Initialize the Variables
-let songIndex = 0;
-let audioElement = new Audio('songs/1.mp3');
-let masterPlay = document.getElementById('masterPlay');
-let myProgressBar = document.getElementById('myProgressBar');
-let gif = document.getElementById('gif');
-let masterSongName = document.getElementById('masterSongName');
-let songItems = Array.from(document.getElementsByClassName('songItem'));
+let playpause_btn = document.querySelector('.playpause-track');
+let next_btn = document.querySelector('.next-track');
+let prev_btn = document.querySelector('.prev-track');
 
-let songs = [
-    {songName: "Cheetay Ka Jigar ", filePath: "songs/1.mp3", coverPath: "covers/1.png"},
-    {songName: "Hum Ashab Ke Khadim", filePath: "songs/2.mp3", coverPath: "covers/2.png"},
-    {songName: "Shaheen ", filePath: "songs/3.mp3", coverPath: "covers/3.png"},
-    {songName: " Hum Ko Maloom Hai ", filePath: "songs/4.mp3", coverPath: "covers/4.png"},
-    {songName: " Teri Manzil Hy ", filePath: "songs/5.mp3", coverPath: "covers/5.png"},
-    {songName: "Hum Ko Mita Sake Ya ", filePath: "songs/2.mp3", coverPath: "covers/6.png"},
-    {songName: "Chalna Para Hai ", filePath: "songs/2.mp3", coverPath: "covers/7.png"},
-    {songName: "Aay Dushman E Deen ", filePath: "songs/2.mp3", coverPath: "covers/8.png"},
-    {songName: "Dil Mein Shahadat ", filePath: "songs/2.mp3", coverPath: "covers/9.png"},
-    {songName: "Labaik Ya Aqsa", filePath: "songs/10.mp3", coverPath: "covers/10.png"},
-    {songName: "AL Qusu lan labaik ", filePath: "songs/11.mp3", coverPath: "covers/11.png"},
-    {songName: " Wo Deen e Muhammad Ke", filePath: "songs/12.mp3", coverPath: "covers/12.png"},
-]
+let seek_slider = document.querySelector('.seek_slider');
+let volume_slider = document.querySelector('.volume_slider');
+let curr_time = document.querySelector('.current-time');
+let total_duration = document.querySelector('.total-duration');
+let wave = document.getElementById('wave');
+let randomIcon = document.querySelector('.fa-random');
+let curr_track = document.createElement('audio');
 
-songItems.forEach((element, i)=>{ 
-    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
-})
- 
+let track_index = 0;
+let isPlaying = false;
+let isRandom = false;
+let updateTimer;
 
-// Handle play/pause click
-masterPlay.addEventListener('click', ()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-        gif.style.opacity = 1;
-    }
-    else{
-        audioElement.pause();
-        masterPlay.classList.remove('fa-pause-circle');
-        masterPlay.classList.add('fa-play-circle');
-        gif.style.opacity = 0;
-    }
-})
-// Listen to Events
-audioElement.addEventListener('timeupdate', ()=>{ 
-    // Update Seekbar
-    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
-    myProgressBar.value = progress;
-})
+const music_list = [
+    {
+        img : 'images/1.png',
+        name : 'Cheetay Ka Jigar',
+        artist : 'Abdullaha mahbob',
+        music : 'music/1.mp3'
+    },
+    {
+        img : 'images/2.png',
+        name : 'Hum Ashab Ke Khadim',
+        artist : 'Siddique umer Waliya',
+        music : 'music/2.mp3'
+    },
+    {
+        img : 'images/3.png',
+        name : 'Shaheen',
+        artist : 'Muaviya Bin Azam',
+        music : 'music/3.mp3'
+    },
+    {
+        img : 'images/4.png',
+        name : 'Hum Ko Maloom Hai',
+        artist : 'Hafeez ur rahman',
+        music : 'music/4.mp3'
+    },
+    {
+        img : 'images/5.png',
+        name : 'Teri Manzil Hai yaha ',
+        artist : 'Eman Club',
+        music : 'music/5.mp3',
+        
+    },
+    {
+        img : 'images/6.png',
+        name : 'Hum Ko Mita Sake Ya',
+        artist : 'Sibghatullah Iqbal',
+        music : 'music/6.mp3'
+    },
+    {
+        img : 'images/7.png',
+        name : 'Chalna Para Hai',
+        artist : 'Hafiz Hassan Anzar',
+        music : 'music/7.mp3'
+    },
+    {
+        img : 'images/8.png',
+        name : 'Aay Dushman E Deen',
+        artist : 'Salahuddin Al Maroofi -',
+        music : 'music/8.mp3'
+    },
+    {
+        img : 'images/9.png',
+        name : 'SDil Mein Shahadat',
+        artist : 'Abdulla Mahboob',
+        music : 'music/9.mp3'
+    },
+    {
+        img : 'images/10.png',
+        name : 'Labaik Ya Aqsa',
+        artist : 'Muaviya Bin Azam',
+        music : 'music/10.mp3'
+    },
+   
+];
 
-myProgressBar.addEventListener('change', ()=>{
-    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
-})
+loadTrack(track_index);
 
-const makeAllPlays = ()=>{
-    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-        element.classList.remove('fa-pause-circle');
-        element.classList.add('fa-play-circle');
-    })
+function loadTrack(track_index){
+    clearInterval(updateTimer);
+    reset();
+
+    curr_track.src = music_list[track_index].music;
+    curr_track.load();
+
+    track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
+    track_name.textContent = music_list[track_index].name;
+    track_artist.textContent = music_list[track_index].artist;
+    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+
+    updateTimer = setInterval(setUpdate, 1000);
+
+    curr_track.addEventListener('ended', nextTrack);
+    random_bg_color();
 }
 
-Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.addEventListener('click', (e)=>{ 
-        makeAllPlays();
-        songIndex = parseInt(e.target.id);
-        e.target.classList.remove('fa-play-circle');
-        e.target.classList.add('fa-pause-circle');
-        audioElement.src = `songs/${songIndex+1}.mp3`;
-        masterSongName.innerText = songs[songIndex].songName;
-        audioElement.currentTime = 0;
-        audioElement.play();
-        gif.style.opacity = 1;
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-    })
-})
+function random_bg_color(){
+    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
+    let a;
 
-document.getElementById('next').addEventListener('click', ()=>{
-    if(songIndex>=9){
-        songIndex = 0
+    function populate(a){
+        for(let i=0; i<6; i++){
+            let x = Math.round(Math.random() * 14);
+            let y = hex[x];
+            a += y;
+        }
+        return a;
     }
-    else{
-        songIndex += 1;
-    }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
-    masterPlay.classList.remove('fa-play-circle');
-    masterPlay.classList.add('fa-pause-circle');
+    let Color1 = populate('#');
+    let Color2 = populate('#');
+    var angle = 'to right';
 
-})
+    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
+    document.body.style.background = gradient;
+}
+function reset(){
+    curr_time.textContent = "00:00";
+    total_duration.textContent = "00:00";
+    seek_slider.value = 0;
+}
+function randomTrack(){
+    isRandom ? pauseRandom() : playRandom();
+}
+function playRandom(){
+    isRandom = true;
+    randomIcon.classList.add('randomActive');
+}
+function pauseRandom(){
+    isRandom = false;
+    randomIcon.classList.remove('randomActive');
+}
+function repeatTrack(){
+    let current_index = track_index;
+    loadTrack(current_index);
+    playTrack();
+}
+function playpauseTrack(){
+    isPlaying ? pauseTrack() : playTrack();
+}
+function playTrack(){
+    curr_track.play();
+    isPlaying = true;
+    track_art.classList.add('rotate');
+    wave.classList.add('loader');
+    playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+function pauseTrack(){
+    curr_track.pause();
+    isPlaying = false;
+    track_art.classList.remove('rotate');
+    wave.classList.remove('loader');
+    playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+}
+function nextTrack(){
+    if(track_index < music_list.length - 1 && isRandom === false){
+        track_index += 1;
+    }else if(track_index < music_list.length - 1 && isRandom === true){
+        let random_index = Number.parseInt(Math.random() * music_list.length);
+        track_index = random_index;
+    }else{
+        track_index = 0;
+    }
+    loadTrack(track_index);
+    playTrack();
+}
+function prevTrack(){
+    if(track_index > 0){
+        track_index -= 1;
+    }else{
+        track_index = music_list.length -1;
+    }
+    loadTrack(track_index);
+    playTrack();
+}
+function seekTo(){
+    let seekto = curr_track.duration * (seek_slider.value / 100);
+    curr_track.currentTime = seekto;
+}
+function setVolume(){
+    curr_track.volume = volume_slider.value / 100;
+}
+function setUpdate(){
+    let seekPosition = 0;
+    if(!isNaN(curr_track.duration)){
+        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+        seek_slider.value = seekPosition;
 
-document.getElementById('previous').addEventListener('click', ()=>{
-    if(songIndex<=0){
-        songIndex = 0
+        let currentMinutes = Math.floor(curr_track.currentTime / 60);
+        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(curr_track.duration / 60);
+        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+        if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
+        if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
+        if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
     }
-    else{
-        songIndex -= 1;
-    }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
-    masterPlay.classList.remove('fa-play-circle');
-    masterPlay.classList.add('fa-pause-circle');
-})
+}
